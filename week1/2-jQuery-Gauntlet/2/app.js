26dbca32556622c6a96a13bd508914450d182db3
Krasimira Badova <krasi@hacksoft.io>
2016-09-29 12:04:16 +0300

    Add 2 task

--- week1/2-jQuery-Gauntlet/2/app.js
+++ week1/2-jQuery-Gauntlet/2/app.js
@@ -1,1 +1,8 @@
-console.log("Empty I am.");
+$(document).ready(function(){
+    var temple = $('#mightyParagraphHolder').children().first();
+        $('button').on('click', function(){
+        temple.prev().removeClass('highlight');
+        temple.addClass('highlight');
+        temple = temple.next();
+    });
+});
