df9453ac8d20e391892804961a7601a9d11750d6
Krasimira Badova <krasi@hacksoft.io>
2016-09-29 13:48:17 +0300

    Add task3

--- week1/2-jQuery-Gauntlet/3/app.js
+++ week1/2-jQuery-Gauntlet/3/app.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
+     $('#search-button').on('click', function(){
+        var url = $('#search-input').val();
+        if (checkUrl(url)){
+        var img = $('<img src="' + url + '" class="imgClass">');
 
+        $('.col-xs-12').append(img);
+        $('.imgClass').fadeIn('slow');
+    } else{
+        alert("Not a valid image url")
+    }
+    });
+     $('body').on('click', 'img', function(){
+        $(this).remove();
+    });
 });
+
+function checkUrl(url){
+    var pattern = /^(http|https)?:\/\/[a-zA-Z0-9-\.]+\.[a-z]{2,4}/;
+    return url.match(pattern);
+}
