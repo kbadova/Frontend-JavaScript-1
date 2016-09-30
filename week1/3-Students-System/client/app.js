a2750188e0c7aeeefd2fb6f3cdca6013c71617e9
Krasimira Badova <krasi@hacksoft.io>
2016-09-30 18:20:09 +0300

    FInish week1

--- week1/3-Students-System/client/app.js
+++ week1/3-Students-System/client/app.js
@@ -1,22 +1,40 @@
 "use strict";
 
 $(document).ready(function() {
-  alert("Hooray, everything runs ok. You can remove this annoying alert from the code.");
 
   $.getJSON('http://localhost:3000/students', function(students, textStatus) {
-      console.log(textStatus);
-      console.log(students);
-
-      alert("Students came. Open your console and remove this alert!");
-      //start here
+    $.each(students, function(index){
+     var html = '<tr><td>' +  (parseInt(index) + 1) + '</td> <td>' + students[index]['name'] + '</td> <td> '+ students[index]['course'] +' </td> </tr>';
+      $('tbody').append(html);
+    })
   });
 
+  function getData(cb){
+    var allCourses = {};
+    $.getJSON('http://localhost:3000/students', function(students, textStatus) {
+      $.each(students, function(index){
+        if (allCourses[students[index]['course']]){
+          allCourses[students[index]['course']].push(students[index]['name']); 
+        } else{
+         allCourses[students[index]['course']]=[students[index]['name']] ;
+          }
+        });
+      cb(allCourses)
+    });
+  }
   $("#group-btn").on("click", function() {
-    alert("What are you looking at? Go implement that logic.");
-  });
-
-  $("#search-btn").on("click", function() {
-    var searched = $("#search-box").val();
-    alert("You searched for " + searched);
+    getData(function(allCourses){
+      $.each(allCourses, function(index){
+        var filteredBy = '<p>' + index + '</p>';
+        $("#group-btn").closest('div').append(filteredBy);
+        var table = '<table class="table filteredTable"><thead><th>Id</th> <th>Name</th</thead</table>';
+        $("#group-btn").closest('div').append(table);
+        var listWithPeopleOnThatCourse = allCourses[index];
+        $.each(listWithPeopleOnThatCourse, function(index){
+          var html = '<tr><td>' +  (parseInt(index) + 1) + '</td> <td>' + listWithPeopleOnThatCourse[index] + '</td></tr>';
+          $('.filteredTable').last().append(html);
+        })
+      })
+    });
   });
 });
